2eacf723d7f856f5e5fe2f78c34bd53202f0afd8
Albert <github@albert.sh>
2020-02-18 02:09:43 +0800

    Simplify

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
-BASEDIR=$(pwd)
 mkdir -p gen
 mkdir -p bin
 mkdir -p bin/frontend
 ant
-(cd src && javac test/LexerTests.java && java -cp .:$BASEDIR/lib/junit-4.13.jar:$BASEDIR/lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore test.LexerTests)
+BASEDIR=$(pwd)
+CLASSPATH=".:$BASEDIR/lib/junit-4.13.jar:$BASEDIR/lib/hamcrest-core-1.3.jar"
+(cd src && javac -cp $CLASSPATH test/LexerTests.java && java -cp $CLASSPATH org.junit.runner.JUnitCore test.LexerTests)
