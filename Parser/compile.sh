f189e6d5ff840631b42457661f4f6b8192953762
Albert <github@albert.sh>
2020-03-03 23:03:46 +0800

    Link runtime only

--- Parser/compile.sh
+++ Parser/compile.sh
@@ -3,5 +3,5 @@ mkdir -p bin
 mkdir -p bin/frontend
 ant
 BASEDIR=$(pwd)
-CLASSPATH=".:$BASEDIR/lib/beaver-ant.jar:$BASEDIR/lib/beaver-rt.jar:$BASEDIR/../junit/junit-4.13.jar:$BASEDIR/../junit/hamcrest-core-1.3.jar"
+CLASSPATH=".:$BASEDIR/lib/beaver-rt.jar:$BASEDIR/../junit/junit-4.13.jar:$BASEDIR/../junit/hamcrest-core-1.3.jar"
 (cd src && javac -cp $CLASSPATH test/ParserTests.java && java -cp $CLASSPATH org.junit.runner.JUnitCore test.ParserTests)
